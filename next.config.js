2cbfabf02860edaabdc14802750ba3f041f98df0
ASgmbv <alimbeksagymbaev@gmail.com>
2021-04-04 16:15:29 +0600

    added callback widget

--- next.config.js
+++ next.config.js
@@ -4,3 +4,11 @@
 //   },
 // };
 module.exports = { trailingSlash: true };
+
+// let widgetHash = '2xhiz0orgjlned4wesn1';
+// let gcw = document.createElement('script');
+// gcw.type = 'text/javascript';
+// gcw.async = true;
+// gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
+// let sn = document.getElementsByTagName('script')[0];
+// sn.parentNode.insertBefore(gcw, sn);
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,28 @@ import Slider from "../components/Slider";
 import Mission from "../components/Mission";
 import PlansTabs from "../components/PlansTabs";
 import GuarantessGrid from "../components/GuarantessGrid";
+import NextHead from "next/head";
 
 export default function Home() {
   return (
     <>
       <Layout title="Ихсан Груп">
+        <NextHead>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                let widgetHash = '2xhiz0orgjlned4wesn1';
+                let gcw = document.createElement('script');
+                gcw.type = 'text/javascript';
+                gcw.async = true;
+                gcw.src = '//widgets.binotel.com/getcall/widgets/'+ widgetHash +'.js';
+                let sn = document.getElementsByTagName('script')[0];
+                sn.parentNode.insertBefore(gcw, sn);
+          `,
+            }}
+          />
+        </NextHead>
+
         <Slider />
 
         <Container maxW="container.lg2" py={["50px", null, "100px"]}>
